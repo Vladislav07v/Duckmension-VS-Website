f3488eefd80c3759a2e4d71559b5ef8716c972fa
Vladislav07v <whdfcr@gmail.com>
2026-04-18 09:37:17 +0300

    Sorting, user searching functionality, user settings and more work on the Ui

--- Duckmension-Website/Controllers/HomeController.cs
+++ Duckmension-Website/Controllers/HomeController.cs
@@ -1,16 +1,23 @@
 using Duckmension_Website.Models;
+using Duckmension_Website.Models.ViewModels;
+using Duckmension_Website.Data;
 using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
 using System.Diagnostics;
+using System.Linq;
+using System.Threading.Tasks;
 
 namespace Duckmension_Website.Controllers
 {
     public class HomeController : Controller
     {
         private readonly ILogger<HomeController> _logger;
+        private readonly ApplicationDbContext _db;
 
-        public HomeController(ILogger<HomeController> logger)
+        public HomeController(ILogger<HomeController> logger, ApplicationDbContext db)
         {
             _logger = logger;
+            _db = db;
         }
 
         public IActionResult Index()
@@ -23,9 +30,21 @@ namespace Duckmension_Website.Controllers
             return View();
         }
 
-        public IActionResult UserFind()
+        public async Task<IActionResult> UserFind()
         {
-            return View();
+            // Load user profiles including the linked Identity user and project to a strongly-typed model
+            var profiles = await _db.UserProfiles
+                .Include(p => p.User)
+                .ToListAsync();
+
+            var model = profiles.Select(p => new UserProfileListItemViewModel
+            {
+                UserName = p.User?.UserName ?? p.UserId,
+                Cookies = p.CookieCount,
+                Hats = p.CurrentlyWornHat
+            }).ToList();
+
+            return View(model);
         }
 
         [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
